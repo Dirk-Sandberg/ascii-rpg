06a708c415fe832d677284cafbee9cbcc1b030f0
erik sandberg <eriksandbergum@gmail.com>
2021-10-30 15:02:53 -0500

    Player status update better

--- player.py
+++ player.py
@@ -4,15 +4,15 @@ from kivy.properties import NumericProperty
 class Creature(Widget):
     name = ''
     current_health = NumericProperty(100)
-    max_health = 100
-    number_of_attacks = 1
-    attack = 1
-    defense = 0
-    crit_chance = 0
-    crit_multiplier = 0
-    lifesteal = 0
-    evasion = 0
-    accuracy = 100
+    max_health = NumericProperty(100)
+    number_of_attacks = NumericProperty(1)
+    attack = NumericProperty(1)
+    defense = NumericProperty(0)
+    crit_chance = NumericProperty(0)
+    crit_multiplier = NumericProperty(0)
+    lifesteal = NumericProperty(0)
+    evasion = NumericProperty(0)
+    accuracy = NumericProperty(100)
 
     def __init__(self, name = '',
                  max_health=100,
