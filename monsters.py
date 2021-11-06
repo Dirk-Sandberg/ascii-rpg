652323b8a49bb09bf07b6b67af497b8e4ba9634b
erik sandberg <eriksandbergum@gmail.com>
2021-11-06 13:36:33 -0500

    more monsters, load them in better fashion

--- monsters.py
+++ monsters.py
@@ -1,20 +1,21 @@
 from player import Creature
 from elements import elements
 import random
-from kivy.properties import NumericProperty
-# with open("art/spider.txt", "r") as f:
-#     spider_art = "".join(line for line in f.readlines())
-monsters_and_art = {
-    'spider': {
-        'art': 'spider',
-        'credit': 'Joan Stark',
-    },
-    # 'yeti': 'i am a rat',
-    # 'golem': 'i am a rat',
-    # 'colossus': 'i am a rat',
-    # 'titan': 'i am a rat',
-    # 'demon': 'i am a rat'
-}
+import os
+
+monsters_folder = "art/monsters"
+monster_files = os.listdir(monsters_folder)
+monsters = []
+for monster_file in monster_files:
+    with open(f"{monsters_folder}/{monster_file}", "r") as f:
+        monster_name = monster_file.replace('.txt', '')
+        file_contents = f.read()
+        monsters.append({
+            'name': monster_name,
+            'art': file_contents[:file_contents.find('credit')],
+            'credit': file_contents[file_contents.find('credit')+len('credit\n'):]
+        })
+
 class Monster(Creature):
     element = ''
     art = '(>".")>'
@@ -26,14 +27,10 @@ class Monster(Creature):
 
     def choose_new_monster(self, floor):
         element = random.choice(list(elements.keys()))
-        name = random.choice(list(monsters_and_art.keys()))
+        monster = random.choice(monsters)
         self.attack = random.choice(range(1, 100))
         self.max_health = random.choice(range(1, 100))
         self.current_health = self.max_health
-        self.name = name
+        self.name = monster['name']
         self.element = element
-        self.art = load_txt_art(monsters_and_art[name]['art'])
-
-def load_txt_art(name):
-    with open(f"art/monsters/{name}.txt", "r") as f:
-        return f.read()
+        self.art = monster['art']
